4a563c49ed3397a0778b8be702506eb4dd2a7f18
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 10:19:37 +0200

    Add health check package (api) and SignalR client package (client)

--- SmartX.Client/obj/Debug/net10.0/SmartX.Client.AssemblyInfo.cs
+++ SmartX.Client/obj/Debug/net10.0/SmartX.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43136f09d1f67c0a9ab5f957370cbed47689019")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
