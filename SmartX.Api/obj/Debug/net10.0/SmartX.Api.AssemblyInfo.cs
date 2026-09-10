bc14ba539ab8596959ca35727769dc7ea393cbd7
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 10:23:47 +0200

    Add DeploymentNode entity for nested trees

--- SmartX.Api/obj/Debug/net10.0/SmartX.Api.AssemblyInfo.cs
+++ SmartX.Api/obj/Debug/net10.0/SmartX.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartX.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883faa348e72a85234d4a83c47a3c1d0f5e0f565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b39b8fae62af90c4b61a77de844ba596164c150")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartX.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartX.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
