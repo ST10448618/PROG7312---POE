cd172ec5f38b5df2c6b46d0800c6f57cf076a224
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 10:29:14 +0200

    Add ==, !=, IEquatable, IComparable to PowerReading

--- SmartX.Api/Domain/ValueObjects/PowerReading.cs
+++ SmartX.Api/Domain/ValueObjects/PowerReading.cs
@@ -22,4 +22,16 @@ public class PowerReading : IComparable<PowerReading>, IEquatable<PowerReading>
     public static bool operator <(PowerReading a, PowerReading b) => a.Watts < b.Watts;
     public static bool operator >=(PowerReading a, PowerReading b) => a.Watts >= b.Watts;
     public static bool operator <=(PowerReading a, PowerReading b) => a.Watts <= b.Watts;
+
+     public static bool operator ==(PowerReading? a, PowerReading? b)
+        => a is null ? b is null : a.Equals(b);
+    public static bool operator !=(PowerReading? a, PowerReading? b) => !(a == b);
+
+    public int CompareTo(PowerReading? other) => other is null ? 1 : Watts.CompareTo(other.Watts);
+    public bool Equals(PowerReading? other) =>
+        other is not null && DeviceId == other.DeviceId && Math.Abs(Watts - other.Watts) < 0.0001;
+    public override bool Equals(object? obj) => Equals(obj as PowerReading);
+    public override int GetHashCode() => HashCode.Combine(DeviceId, Watts);
+
+    public override string ToString() => $"{DeviceId}: {Watts:0.0}W";
 }
