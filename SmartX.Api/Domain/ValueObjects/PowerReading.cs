3e8e0b01735be065f50d59dbc548167eb4500a95
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 10:27:50 +0200

    Add PowerReading with +/- operators

---
+++ SmartX.Api/Domain/ValueObjects/PowerReading.cs
@@ -0,0 +1,22 @@
+namespace SmartX.Api.Domain.ValueObjects;
+
+public class PowerReading : IComparable<PowerReading>, IEquatable<PowerReading>
+{
+    public string DeviceId { get; set; } = "";
+    public double Watts { get; set; }
+
+    public PowerReading() { }
+    public PowerReading(string deviceId, double watts)
+    {
+        DeviceId = deviceId;
+        Watts = watts;
+    }
+
+    public static PowerReading operator +(PowerReading a, PowerReading b)
+        => new($"{a.DeviceId}+{b.DeviceId}", a.Watts + b.Watts);
+
+    public static PowerReading operator -(PowerReading a, PowerReading b)
+        => new($"{a.DeviceId}-{b.DeviceId}", Math.Abs(a.Watts - b.Watts));
+
+
+}
