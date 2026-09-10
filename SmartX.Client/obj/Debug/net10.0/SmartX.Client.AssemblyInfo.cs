dff23efd63a0e8746f762728bb0308d40fcc0cc7
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 13:23:30 +0200

    Add Ingestion.razor with registration, upload, path validator

--- SmartX.Client/obj/Debug/net10.0/SmartX.Client.AssemblyInfo.cs
+++ SmartX.Client/obj/Debug/net10.0/SmartX.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9f8f94983c978cd44b02e0a45fc9379f456bfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad850973ae7d1ff9a45044e4b6bbcf1e98353c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartX.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
