237653398e4c7c3dace2908b812087976a880395
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-10 19:32:28 +0200

    Add ConnectionTracker, increments/decrements on real SignalR connect/disconnect

--- SmartX.Api/Hubs/TelemetryHub.cs
+++ SmartX.Api/Hubs/TelemetryHub.cs
@@ -2,8 +2,28 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SmartX.Api.Hubs;
 
+public class ConnectionTracker
+{
+    private int _count;
+    public int Count => _count;
+    public void Increment() => Interlocked.Increment(ref _count);
+    public void Decrement() => Interlocked.Decrement(ref _count);
+}
+
 public class TelemetryHub : Hub
 {
-    public async Task BroadcastPulse(string sensorId, string status)
-        => await Clients.All.SendAsync("SensorPulse", sensorId, status);
+    private readonly ConnectionTracker _tracker;
+    public TelemetryHub(ConnectionTracker tracker) => _tracker = tracker;
+
+    public override Task OnConnectedAsync()
+    {
+        _tracker.Increment();
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _tracker.Decrement();
+        return base.OnDisconnectedAsync(exception);
+    }
 }
