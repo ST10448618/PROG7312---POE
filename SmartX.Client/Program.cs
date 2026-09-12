24c0f91a46470d92b6c237a1c55f62fbf147ca4c
Perezr12 <ST10448618@vcconnect.edu.za>
2026-09-12 15:52:43 +0200

    Minor UI and backend issues fixed final docker compose file done

--- SmartX.Client/Program.cs
+++ SmartX.Client/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5000";
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
+builder.Services.AddSingleton(new ApiConfig(apiBaseUrl));
 builder.Services.AddScoped<SensorApiClient>();
 
 await builder.Build().RunAsync();
